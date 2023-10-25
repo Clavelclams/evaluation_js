5c67ad3fe524b74846d057adf3ac5de528d6a62a
clavelclams <clavelclams12@gmail.com>
2023-10-25 10:56:57 +0200

    2.03 la variable i etait local à la boucle console log avait pas accés

--- exercice2/exo2.js
+++ exercice2/exo2.js
@@ -2,7 +2,7 @@ function tableMultiplication(nombre){
     for (var i = 1; i <= 10; i++) {
         var resultat = i * nombre;
         alert(i+' x '+nombre+' = '+resultat);
+        console.log(i+' x '+nombre+' = '+resultat);
     }
 }
 tableMultiplication(7);
-console.log(i+' x '+nombre+' = '+resultat);
