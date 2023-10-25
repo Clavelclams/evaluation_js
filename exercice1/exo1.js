6d00d389cac742c2e1616c49ba8b7c428269e86f
clavelclams <clavelclams12@gmail.com>
2023-10-25 09:50:09 +0200

    1.1 Evaluation js exercice 1 terminé

--- exercice1/exo1.js
+++ exercice1/exo1.js
@@ -1,4 +1,28 @@
-var age = parseInt (prompt('Entrer un âge :'));
+var age ;
 var jeunes = 0;
 var moyens = 0;
 var vieux = 0;
+var centenaires = 0;
+while (true) {
+    age = parseInt (prompt('Entrer un âge :'));
+    if (age === 0) {
+        break
+    }  else if (age >= 1 && age < 20) {
+        jeunes++
+    }  else if (age >= 20 && age <= 40) {
+        moyens++
+    }  else if (age > 40 && age < 100) {
+        vieux++
+    }  else if (age >= 100) {
+        centenaires++
+    }
+}
+alert('Nombre de jeunes : '+jeunes);
+alert('Nombre de moyens : '+moyens);
+alert('Nombre de vieux : '+vieux);
+// les alertes  s'affichent pas mais le calcul se fait dans console
+alert('Nombre de centenaire : '+centenaires);
+console.log('Nombre de jeunes : '+jeunes);
+console.log('Nombre de moyens : '+moyens);
+console.log('Nombre de vieux : '+vieux);
+console.log('Nombre de centenaire : '+centenaires);
