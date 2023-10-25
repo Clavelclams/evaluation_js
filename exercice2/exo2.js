7a4a412119ee4e6aee0368769acb6aa1b18378ad
clavelclams <clavelclams12@gmail.com>
2023-10-25 10:10:00 +0200

    2.1 exo 2 la page rien ne se passe

--- exercice2/exo2.js
+++ exercice2/exo2.js
@@ -1,1 +1,6 @@
-function tableMultiplication(7)
+function tableMultiplication(7){
+    for (var i = 1; i <= 10; i++) {
+        var resultat = i * 7;
+        console.log(i+'x'+7+'='+resultat);
+    }
+}
