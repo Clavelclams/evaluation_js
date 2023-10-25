0daca8fbe28ac5bbc5e8285c8ab711318392c787
clavelclams <clavelclams12@gmail.com>
2023-10-25 15:34:08 +0200

    4.2 "Unexpected token" ? probleme d'affichage même dans console l7 msg d'erreur s'affiche

--- exercice4/exo4.js
+++ exercice4/exo4.js
@@ -8,4 +8,13 @@ if (TOT >= 100 && TOT <= 200){
 } else if (TOT > 200) {
     REM = TOT * 10% ;
 }
+if (TOT <= 500) {
+    PORT = Math.max(6, TOT * 2%);
+}
 var PAP = TOT - REM + PORT;
+console.log("Prix unitaire : " + PU + " euros");
+console.log("Quantité commandée : " + QTECOM);
+console.log("Total avant remise et frais de port : " + TOT + " euros");
+console.log("Remise : " + REM + " euros");
+console.log("Frais de port : " + PORT + " euros");
+console.log("Prix à payer : " + PAP + " euros");
