5cf79b9f959618cc0d0407c56bc268b304018567
clavelclams <clavelclams12@gmail.com>
2023-10-25 14:27:20 +0200

    3.4 exo 3 corrigé il manquait la variable index, ma condition if était pas imbriqué dans le fun

--- exercice3/exo3.js
+++ exercice3/exo3.js
@@ -1,13 +1,17 @@
-var tab =  ["Audrey", "Aurélien", "Flavien", "Jérémy", "Laurent", "Melik", "Nouara", "Salem", "Samuel", "Stéphane", " "];
-var pR = prompt('entrez le prénom à rechercher :');
-function searchanddeletename(prenom,tableau){
-    var index = 
-    tableau.indexof(prenom)
-}if (index !== -1) {
-    tableau.splice(index, 1); 
-    console.log("Prénom trouvé et supprimé !");
-} else {
-    console.log("Prénom non trouvé. Aucune suppression.");
+var tab = ["Audrey", "Aurélien", "Flavien", "Jérémy", "Laurent", "Melik", "Nouara", "Salem", "Samuel", "Stéphane", " "];
+var pR = prompt('Entrez le prénom à rechercher (attention au majuscule):');
+
+function searchAndDeleteName(prenom, tableau) {
+    var index = tableau.indexOf(prenom);
+    if (index !== -1) {
+        tableau.splice(index, 1);
+        tableau[tableau.length - 1] = " ";
+        alert('Prénom trouvé et supprimé !');
+        console.log('Prénom trouvé et supprimé !');
+    } else {
+        alert('Prénom non trouvé ! Donc aucune suppression.');
+        console.log('Prénom non trouvé. Donc aucune suppression.');
+    }
 }
 
-searchanddeletename(pR,tab);
+searchAndDeleteName(pR, tab);
