fcce1161233fc8d3915a0aee970412a0f6d9951d
clavelclams <clavelclams12@gmail.com>
2023-10-25 14:10:53 +0200

    3.2 rien ne se passe

--- exercice3/exo3.js
+++ exercice3/exo3.js
@@ -1,1 +1,13 @@
-var tab =  ["Audrey", "Aurélien", "Flavien", "Jérémy", "Laurent", "Melik", "Nouara", "Salem", "Samuel", "Stéphane"];
+var tab =  ["Audrey", "Aurélien", "Flavien", "Jérémy", "Laurent", "Melik", "Nouara", "Salem", "Samuel", "Stéphane", " "];
+var pR = prompt('entrez le prénom à rechercher :');
+function searchanddeletename(prenom,tableau){
+    var index = 
+    tableau.indexof(prenom)
+}if (index !== -1) {
+    tableau.splice(index, 1); 
+    console.log("Prénom trouvé et supprimé !");
+} else {
+    console.log("Prénom non trouvé. Aucune suppression.");
+}
+
+searchanddeletename(pR,tab);
