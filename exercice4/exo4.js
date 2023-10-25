8b038b171df7b88a10db038f56b4738362a19a99
clavelclams <clavelclams12@gmail.com>
2023-10-25 15:29:09 +0200

    4.1 Rien ne s'affiche :/

--- exercice4/exo4.js
+++ exercice4/exo4.js
@@ -1,1 +1,11 @@
 var PU = parseFloat(prompt('Saisissez le prix unitaire du produit (en euro) '));
+var QTECOM = parseInt(prompt('Saisissez la quantité commandée : '));
+var TOT = PU * QTECOM;
+var REM = 0;
+var PORT = 0;
+if (TOT >= 100 && TOT <= 200){
+    REM = TOT * 5% ;
+} else if (TOT > 200) {
+    REM = TOT * 10% ;
+}
+var PAP = TOT - REM + PORT;
