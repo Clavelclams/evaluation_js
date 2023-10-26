8dc28c48f6044b982d6892492b038ea9fbe50ee6
clavelclams <clavelclams12@gmail.com>
2023-10-26 14:18:25 +0200

    5.4 le champs qui dit à l'utilisateur s'active par defaut sans js avec required pour rajouter du champs en js j'ai enlever required

--- exercice5/exo5.js
+++ exercice5/exo5.js
@@ -1,5 +1,23 @@
 document.getElementById('formulaire').addEventListener('submit', function(e) {
-    e.preventDefault();
+    e.preventDefault(); 
+    var erreur;
+    var nom = document.getElementById('nom');
+    var prenom = document.getElementById('prenom');
+    var dtn = document.getElementById('date_naissance');
+    var cp = document.getElementById('code_postale');
+    var email = document.getElementById('email');
+    var checkbox = document.getElementById('accepter');
+
+    if (!nom.value) {
+        erreur = 'Vuillez renseigner votre nom'
+    } 
+    if (erreur) {
+        e.preventDefault();
+        document.getElementById('erreur').innerHTML = erreur;
+    }else {
+        alert('Formulaire envoyé !');
+    }
     console.log('Formulaire soumis !');
-    alert('Formulaire envoyé !');
+    // alert('Formulaire envoyé !');
 });
+
