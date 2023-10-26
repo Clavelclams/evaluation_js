ee4fdbdc67205331048951c375ad2680387e6a75
clavelclams <clavelclams12@gmail.com>
2023-10-26 15:12:12 +0200

    5.5 mon affichage d'erreur se melange malgré l'affectation erreur de ma variable dans les if

--- exercice5/exo5.js
+++ exercice5/exo5.js
@@ -9,11 +9,27 @@ document.getElementById('formulaire').addEventListener('submit', function(e) {
     var checkbox = document.getElementById('accepter');
 
     if (!nom.value) {
-        erreur = 'Vuillez renseigner votre nom'
+        erreur = 'Veuillez renseigner votre nom';
+    }
+    if (!prenom.value) {
+        erreur = 'Veuillez renseigner votre prenom';
+    }
+    if (!dtn.value) {
+        erreur = 'Veuillez renseigner votre date de naissance';
+    }
+    // if (!cp.value) {
+    //     erreur = 'Veuillez renseigner votre code postal';
+    // }
+    if (!email.value) {
+        erreur = 'Veuillez renseigner votre email';
+    }
+    if (!checkbox.value) {
+        erreur = 'Veuillez accepter le traitement informatique de ce formulaire';
     } 
     if (erreur) {
         e.preventDefault();
         document.getElementById('erreur').innerHTML = erreur;
+        return false;
     }else {
         alert('Formulaire envoyé !');
     }
